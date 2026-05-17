7aaab43e4a02a70df70d5a1271a5222c8d801338
Eric-lab-star <populate1356@gmail.com>
2026-05-18 07:54:17 +0900

    색 바꾸기 과제
    색 바꾸기 과제

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pygame
+from random import randint
 
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("pygame shooter")
+pygame.display.set_caption("colors")
 
 
 def main():
     running = True
     surf = pygame.Surface((100, 150))  # <--- 생성
-    surf.fill("orange")  # <-- 색상 설정
+    surf.fill("white")  # <-- 색상 설정
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        display_surface.fill((0, 0, 0))
+        display_surface.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
         display_surface.blit(surf, (100, 100))  # <-- 화면에 붙이기
         pygame.display.flip()
     pygame.quit()
