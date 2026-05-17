c1746914d7733c6ea294da36431488c2d74c611d
Eric-lab-star <populate1356@gmail.com>
2026-05-18 08:05:46 +0900

    과제: 우주선 이동
    벽에 닿았을 때 방향 바꾸기

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ pygame.display.set_caption("space shooter")
 
 player_path = join("images", "player.png")
 player_surf = pygame.image.load(player_path).convert_alpha()
-player_rect = player_surf.get_frect(
-    center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
-)  # <-- 새로운 코드
+player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 bg_path = join("images", "background.png")
 bg_surf = pygame.transform.scale(
@@ -20,19 +18,22 @@ bg_surf = pygame.transform.scale(
 
 def main():
     running = True
-    direction = 1  # <--- 방향
-    speed = 1  # <--- 속력
+    x = 1
+    y = 1
+    speed = 0.5
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        if (
-            player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0
-        ):  # <-- 방향 바꾸는 조건
-            direction *= -1
+        if player_rect.left <= 0 or player_rect.right >= WINDOW_WIDTH:
+            x *= -1
+        if player_rect.top <= 0 or player_rect.bottom >= WINDOW_HEIGHT:
+            y *= -1
 
-        player_rect.right += speed * direction  # <-- 위치 업데이트
+        player_rect.x += x * speed
+        player_rect.y += y * speed
+
         display_surface.blit(bg_surf, (0, 0))
         display_surface.blit(player_surf, player_rect)  # <--- 위치 업데이트
 
