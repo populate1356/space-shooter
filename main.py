c389363b3886978ace7e5ab74a430e525c19230b
Eric-lab-star <populate1356@gmail.com>
2026-05-18 08:09:08 +0900

    FPS와 Vector

--- main.py
+++ main.py
@@ -8,34 +8,28 @@ pygame.display.set_caption("space shooter")
 
 player_path = join("images", "player.png")
 player_surf = pygame.image.load(player_path).convert_alpha()
-player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+player_rect = player_surf.get_frect(center=(pos))
 
 bg_path = join("images", "background.png")
 bg_surf = pygame.transform.scale(
     pygame.image.load(bg_path).convert_alpha(), (WINDOW_WIDTH, WINDOW_HEIGHT)
 )
 
+clock = pygame.time.Clock()
+
 
 def main():
     running = True
-    x = 1
-    y = 1
-    speed = 0.5
+    speed = pygame.Vector2(20, 10)
     while running:
+        dt = clock.tick(30) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        if player_rect.left <= 0 or player_rect.right >= WINDOW_WIDTH:
-            x *= -1
-        if player_rect.top <= 0 or player_rect.bottom >= WINDOW_HEIGHT:
-            y *= -1
-
-        player_rect.x += x * speed
-        player_rect.y += y * speed
-
+        player_rect.center += speed * dt
         display_surface.blit(bg_surf, (0, 0))
-        display_surface.blit(player_surf, player_rect)  # <--- 위치 업데이트
+        display_surface.blit(player_surf, player_rect)
 
         pygame.display.flip()
     pygame.quit()
