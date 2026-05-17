96d3279e516a92adb6c0d036247945aefcb299f4
Eric-lab-star <populate1356@gmail.com>
2026-05-18 08:14:21 +0900

    방향키 설정하기
    방향키를 설정해서 우주선을 이동시키는  코드

--- main.py
+++ main.py
@@ -21,13 +21,20 @@ clock = pygame.time.Clock()
 
 def main():
     running = True
-    speed = pygame.Vector2(20, 10)
+    speed = 200
+    direction = pygame.Vector2(0, 0)
     while running:
         dt = clock.tick(30) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        player_rect.center += speed * dt
+        keys = pygame.key.get_pressed()
+        direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        if direction.length() > 0:
+            direction.normalize_ip()
+        player_rect.center += direction * speed * dt
+        player_rect.clamp_ip(pygame.Rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
         display_surface.blit(bg_surf, (0, 0))
         display_surface.blit(player_surf, player_rect)
 
